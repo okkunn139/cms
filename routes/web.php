683b100319bce001960317f8504d1fb7b08e7fd3
okkunn139 <okujuny@gmail.com>
2023-08-17 00:03:28 +0900

    cms to chapter 10

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,5 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-use App\Book;
-use Illuminate\Http\Request;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,14 +11,52 @@ use Illuminate\Http\Request;
 |
 */
 
+use App\Book;
+use Illuminate\Http\Request;
+
+/**
+* 本の一覧表示(books.blade.php)
+*/
 Route::get('/', function () {
-    return view('welcome');
+    return view('books');
 });
 
+/**
+* 本を追加 
+*/
 Route::post('/books', function (Request $request) {
     
+    //バリデーション
+    $validator = Validator::make($request->all(), [
+        'item_name' => 'required|max:255',
+    ]);
+    
+    //バリデーション：エラー
+    if ($validator->fails()) {
+        return redirect('/')
+            ->withInput()
+            ->withErrors($validator);
+    }
+    
+    //Eloquent モデル (登録処理)
+    $books = new Book;
+    $books->item_name = $request->item_name;
+    $books->item_number = '1';
+    $books->item_amount = '1000';
+    $books->published = '2017-03-07 00:00:00';
+    $books->save();
+    return redirect('/');
 });
 
+/**
+* 本を削除 
+*/
 Route::delete('/book/{book}', function (Book $book) {
-    
+    //
 });
+
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
